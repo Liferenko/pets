bf6b31e7b66cd0fb1dbe15137cbdc7eec43c7297
Лиференко Павел <liferenko@reg.ru>
2018-03-26 20:10:59 +0300

    добавил кикстартовый код CV

--- py/dudo_cv/visioner.py
+++ py/dudo_cv/visioner.py
@@ -1,1 +1,33 @@
 # -*- coding: utf8 -*-
+
+"""
+Goal:
+to see dice game, check if combinations exists and send data to counter
+
+Mainteiner: Liferenko Pavel
+
+use - https://github.com/madmaze/pytesseract
+"""
+
+try:
+	import Image
+except ImportError:
+	from PIL import Image
+import pytesseract
+
+
+pytesseract.pytesseract.tesseract_cmd = '<full_path_to_your_tesseract_executable>'
+# Include the above line, if you don't have tesseract executable in your PATH
+# Example tesseract_cmd: 'C:\\Program Files (x86)\\Tesseract-OCR\\tesseract'
+
+# Simple image to string
+print(pytesseract.image_to_string(Image.open('test.png')))
+
+# French text image to string
+print(pytesseract.image_to_string(Image.open('test-european.jpg'), lang='fra'))
+
+# Get bounding box estimates
+print(pytesseract.image_to_boxes(Image.open('test.png')))
+
+# Get verbose data including boxes, confidences, line and page numbers
+print(pytesseract.image_to_data(Image.open('test.png'))
