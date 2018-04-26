95829caf69216b29a5a08d4a36128b051501b410
Лиференко Павел <liferenko@reg.ru>
2018-04-27 00:57:25 +0300

    it works from example. TODO add 15min cutting, overrecording and background working

--- py/liferenko/head/brain/memory/short-term/audio_register/src/main/python/mic_recorder.py
+++ py/liferenko/head/brain/memory/short-term/audio_register/src/main/python/mic_recorder.py
@@ -18,25 +18,31 @@ import tempfile
 import queue
 import sys
 
-# optional flags
 def int_or_str(text):
+    """Helper function for argument parsing."""
     try:
         return int(text)
     except ValueError:
         return text
 
 parser = argparse.ArgumentParser(description=__doc__)
-parser.add_argument( 
+parser.add_argument(
     '-l', '--list-devices', action='store_true',
-    help='show list of audio devices and exit'
- )
-parser.add_argument( 
-    'filename', nargs='?',
-    metavar='FILENAME', 
-    help='audio file to store recording to'
- )
+    help='show list of audio devices and exit')
+parser.add_argument(
+    '-d', '--device', type=int_or_str,
+    help='input device (numeric ID or substring)')
+parser.add_argument(
+    '-r', '--samplerate', type=int, help='sampling rate')
+parser.add_argument(
+    '-c', '--channels', type=int, default=1, help='number of input channels')
+parser.add_argument(
+    'filename', nargs='?', metavar='FILENAME',
+    help='audio file to store recording to')
+parser.add_argument(
+    '-t', '--subtype', type=str, help='sound file subtype (e.g. "PCM_24")')
 args = parser.parse_args()
-# END optional flags    
+
         
 # device list
 if args.list_devices:
@@ -46,7 +52,10 @@ if args.list_devices:
 # define filename
 if args.filename is None:
     args.filename = tempfile.mktemp( prefix='rec_', suffix='.wav', dir='result/' )
+    # TODO find how to rec on .mp3 or another less-size-format
+
 # END define filename
+
 
 q = queue.Queue() # FIFO (wanna test with LIFO)
 
@@ -57,12 +66,10 @@ def callback( indata, frames, time, status ):
     q.put( indata.copy() )
 # END ...for each audioblock
 
-# 
-# END 
 
 
 # file saver ('with...as' promises that file will close)
-with soundfile.SoundFile( args.filename, mode='x' ) as file:
+with soundfile.SoundFile( args.filename, mode='x', samplerate=44100, channels=2 ) as file:
     with sounddevice.InputStream( device=args.device, callback=callback ):
         print( '--/' * 11 )
         print( 'Press Ctrl+C to stop the rec' )
